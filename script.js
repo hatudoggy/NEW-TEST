fa3b6c4d3a395837182d3a495ec151e2b2ed2377
Hatdoggy <topers1820@gmail.com>
2023-01-16 13:06:08 +0800

    Fixed coder's art section

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
+const tolerance = 1.9;
 
 productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
 
     nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
+        item.scrollLeft += (290 - (item.scrollLeft % 290));
+        console.log("Scroll Next");
     })
+    
 
     preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
+        let remainder = item.scrollLeft % 290;
+        // If it's not, adjust the scroll amount
+        if (remainder >= tolerance) {
+            item.scrollLeft -= remainder;
+            
+        } else {
+            item.scrollLeft -= 290;
+            
+        }
+        //item.scrollLeft -= (item.scrollLeft % 290);
+ 
     })
-})
+});
+
+
+var scrollAmt;
+const productContainer = document.querySelector('.product-container');
+productContainer.addEventListener("scroll", event =>{
+    scrollAmt = productContainer.scrollLeft;
+    console.log(scrollAmt);
+}, {passive: true});
+
